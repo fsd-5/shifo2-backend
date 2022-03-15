06b680014cca862f784acf1e97be90df29725910
Umida Turdiyeva <umidabunyod@bk.ru>
2022-03-16 00:33:09 +0500

    xonaRepositoryda @Repository qolib ketibdi

--- src/main/java/net/idrok/shifoxona/repository/XonaRepository.java
+++ src/main/java/net/idrok/shifoxona/repository/XonaRepository.java
@@ -1,9 +1,11 @@
 package net.idrok.shifoxona.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import net.idrok.shifoxona.entity.Xona;
 
+@Repository
 public interface XonaRepository extends JpaRepository<Xona, Long> {
     
 }
