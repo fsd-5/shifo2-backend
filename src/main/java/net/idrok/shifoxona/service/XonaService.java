073794e42e1185153cc51bd6f26f2068d1873f79
Umida Turdiyeva <umidabunyod@bk.ru>
2022-03-16 00:13:15 +0500

    XonaService

--- src/main/java/net/idrok/shifoxona/service/XonaService.java
+++ src/main/java/net/idrok/shifoxona/service/XonaService.java
@@ -1,5 +1,8 @@
 package net.idrok.shifoxona.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
 
 import net.idrok.shifoxona.entity.Xona;
 import net.idrok.shifoxona.repository.XonaRepository;
@@ -8,19 +11,17 @@ import net.idrok.shifoxona.repository.XonaRepository;
 public class XonaService {
     private final XonaRepository xonaRepository;
 
-
     public XonaService(XonaRepository xonaRepository) {
         this.xonaRepository = xonaRepository;
     }
 
-    public List<Xona> getAll(){
+    public List<Xona> getAll() {
         return xonaRepository.findAll();
     }
 
-
-    public Xona create(Xona xona){
-        if(xona.getId() == null)
-        return this.xonaRepository.save(xona);
+    public Xona create(Xona xona) {
+        if (xona.getId() == null)
+            return this.xonaRepository.save(xona);
         throw new RuntimeException("ID null bo'lsihi kerak");
     }
 
