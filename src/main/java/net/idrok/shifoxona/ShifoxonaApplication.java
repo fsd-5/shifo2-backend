1d499e8c09acda84690f2a143fed0f8c77941939
Muminniyoz Shonazarov <muminniyoz2912@gmail.com>
2022-03-15 16:43:19 +0500

    Birinchi yuklash

---
+++ src/main/java/net/idrok/shifoxona/ShifoxonaApplication.java
@@ -0,0 +1,13 @@
+package net.idrok.shifoxona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShifoxonaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShifoxonaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/idrok/shifoxona/ShifoxonaApplicationTests.java
@@ -0,0 +1,13 @@
+package net.idrok.shifoxona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShifoxonaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
